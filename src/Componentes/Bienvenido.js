d4843c2c7015d9c73ec0df3775176d7881ce21fe
Carlos Robayo <carlosrobayocabra@gmail.com>
2023-11-21 22:51:40 -0500

    Modificar plantilla bienvenido

--- src/Componentes/Bienvenido.js
+++ src/Componentes/Bienvenido.js
@@ -2,17 +2,15 @@ import "../Estilos/bienvenido.css";
 
 export const Bienvenido = () => {
   return (
-    <div className="bienvenidos">
-      <div className="bg">
-        <div className="appreg">
-          <h1 className="appreg">APP Registro de gastos</h1>
-        </div>
-        <div className="rectangle1">
-          
-        </div>
-        <div className="crearcuenta">
-          <h4>Crear cuenta</h4>
-        </div>
+    <div className="bg">
+      <div className="appreg">
+        <h1 className="appreg">APP Registro de gastos</h1>
+      </div>
+      <div className="rectangle1">
+        <button className="btn">Log in</button>
+      </div>
+      <div className="crearcuenta">
+        <h4>Crear cuenta</h4>
       </div>
     </div>
   );
