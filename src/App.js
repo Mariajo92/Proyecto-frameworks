e8f069b4f8606cbbce189948bdcf727e2fbde84c
unknown <Mara Jos>
2023-11-21 19:53:42 -0500

    Guardar todos los cambios

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import { Bienvenido } from './Componentes/Bienvenido';
+function App() {
+  return (
+    <div className="App">
+      <Bienvenido></Bienvenido>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Componentes/Bienvenido.js
@@ -0,0 +1,19 @@
+import "../Estilos/bienvenido.css";
+
+export const Bienvenido = () => {
+  return (
+    <div className="bienvenidos">
+      <div className="bg">
+        <div className="appreg">
+          <h1 className="appreg">APP Registro de gastos</h1>
+        </div>
+        <div className="rectangle1">
+          
+        </div>
+        <div className="crearcuenta">
+          <h4>Crear cuenta</h4>
+        </div>
+      </div>
+    </div>
+  );
+};
